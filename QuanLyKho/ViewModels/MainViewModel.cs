0d7e5f6a12c959050f41ae1219b67843a9b8d151
Lê Hoàng Trọng <lehoangtrongcsgl@gmail.com>
2024-11-18 16:46:36 +0700

    Add new windows and update existing UI elements
    Updated `QuanLyKho.sln` to change project type GUID for "QuanLyKho".
    Modified `LoginWindow.xaml` to use `x:Name` and updated binding.
    Enhanced `MainWindow.xaml` with new buttons, filter section, and content.
    Added new ICommand properties in `MainViewModel.cs` for various commands.
    Introduced new windows for Customer, Input, Object, Output, Supplier, Unit, and User management with corresponding XAML and code-behind files.

--- QuanLyKho/ViewModels/MainViewModel.cs
+++ QuanLyKho/ViewModels/MainViewModel.cs
@@ -12,14 +12,63 @@ namespace QuanLyKho.ViewModels
     {
         public bool IsLoaded { get; set; }
         public ICommand LoadedWindowCommand { get; set; }
+        public ICommand UnitCommand { get; set; }
+        public ICommand SuplierCommand { get; set; }
+        public ICommand CustomerCommand { get; set; }
+        public ICommand ObjectCommand { get; set; }
+        public ICommand UserCommand { get; set; }
+        public ICommand InputCommand { get; set; }
+        public ICommand OutputCommand { get; set; }
+
         public MainViewModel()
         {
             LoadedWindowCommand = new RelayCommand<Window>((p) => { return p == null ? false : true; }, (p) =>
             {
                 IsLoaded = true;
                 LoginWindow login = new LoginWindow();
-                p.Hide();
                 login.ShowDialog();
+            });
+
+            UnitCommand = new RelayCommand<object>((p) => { return true; }, (p) =>
+            {
+                UnitWindow unitWindow = new UnitWindow();
+                unitWindow.ShowDialog();
+            });
+
+            SuplierCommand = new RelayCommand<object>((p) => { return true; }, (p) =>
+            {
+                SuplierWindow suplierWindow = new SuplierWindow();
+                suplierWindow.ShowDialog();
+            });
+
+            CustomerCommand = new RelayCommand<object>((p) => { return true; }, (p) =>
+            {
+                CustomerWindow customerWindow = new CustomerWindow();
+                customerWindow.ShowDialog();
+            });
+
+            ObjectCommand = new RelayCommand<object>((p) => { return true; }, (p) =>
+            {
+                ObjectWindow objectWindow = new ObjectWindow();
+                objectWindow.ShowDialog();
+            });
+
+            UserCommand = new RelayCommand<object>((p) => { return true; }, (p) =>
+            {
+                UserWindow userWindow = new UserWindow();
+                userWindow.ShowDialog();
+            });
+
+            InputCommand = new RelayCommand<object>((p) => { return true; }, (p) =>
+            {
+                InputWindow inputWindow = new InputWindow();
+                inputWindow.ShowDialog();
+            });
+
+            OutputCommand = new RelayCommand<object>((p) => { return true; }, (p) =>
+            {
+                OutputWindow outputWindow = new OutputWindow();
+                outputWindow.ShowDialog();
             });
         }
     }
